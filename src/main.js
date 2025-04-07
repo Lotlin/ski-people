6015f18512bcf7a5333ef7871208437a28323e0b
Lotlin <lotlin22@gmail.com>
2025-04-07 21:53:14 +0200

    slider

--- src/main.js
+++ src/main.js
@@ -1,2 +1,23 @@
 import 'normalize.css';
 import './style.scss';
+
+import {Navigation, Thumbs} from 'swiper/modules';
+import Swiper from 'swiper';
+
+const thumbnailsSlider = new Swiper('.product__slider-thumbnails', {
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+const productSlider = new Swiper('.product__slider', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.product__slider-arrow--next',
+    prevEl: '.product__slider-arrow--prev',
+  },
+  modules: [Navigation, Thumbs],
+  thumbs: {
+    swiper: thumbnailsSlider,
+  },
+});
